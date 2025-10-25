a2e8f69ca1fd503e3478fc26bc246979cea0266f
someone <idontwantogivemyemailouttopeople@proton.me>
2025-10-25 20:02:07 +0200

    removed unnecessary directories

--- build/CMakeFiles/randomx.dir/src/cpp/reciprocal.c.o.d
+++
@@ -1,9 +0,0 @@
-CMakeFiles/randomx.dir/src/cpp/reciprocal.c.o: \
-  /home/someone/webminer/WebRandomX/src/cpp/reciprocal.c \
-  /usr/lib/emsdk/upstream/emscripten/cache/sysroot/include/assert.h \
-  /usr/lib/emsdk/upstream/emscripten/cache/sysroot/include/features.h \
-  /home/someone/webminer/WebRandomX/src/cpp/reciprocal.h \
-  /usr/lib/emsdk/upstream/lib/clang/20/include/stdint.h \
-  /usr/lib/emsdk/upstream/emscripten/cache/sysroot/include/stdint.h \
-  /usr/lib/emsdk/upstream/emscripten/cache/sysroot/include/bits/alltypes.h \
-  /usr/lib/emsdk/upstream/emscripten/cache/sysroot/include/bits/stdint.h
